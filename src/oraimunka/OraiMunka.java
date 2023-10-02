4d075586815eefb6fcb4ee8254ceef3a71bef8b5
fureszb <fureszb@gmail.com>
2023-10-02 09:37:21 +0200

    keveresNemJo

--- src/oraimunka/OraiMunka.java
+++ src/oraimunka/OraiMunka.java
@@ -4,21 +4,18 @@ import java.util.Scanner;
 
 public class OraiMunka {
 
-    static String[] pakli = new String[22];
-    
+    static String[] pakli = new String[21];
+
     public static void main(String[] args) {
         kirak();
         kiiratas();
-        
+
         for (int i = 0; i < 3; i++) {
             int bekert = bekeres();
             keveres(bekert);
         }
         ezvolt();
-       
 
-        
-        
     }
 
     private static int bekeres() {
@@ -34,45 +31,60 @@ public class OraiMunka {
     }
 
     private static void kiiratas() {
-          int sortores = 0;
+        int sortores = 0;
         for (int i = 0; i < pakli.length; i++) {
-            System.out.printf(pakli[i] + "\t"+"|"+ "\t");
+            System.out.printf("%12s", pakli[i]);
             sortores++;
             if (sortores == 3) {
                 System.out.println("");
-                sortores=0;
+                sortores = 0;
             }
         }
     }
 
     private static void keveres(int oszlop) {
-        if (oszlop == 1) {
-            for (int i = 0; i < 7; i++) {
-                pakli[i] = pakli[20 - i * 3];
-                 pakli[i+7] = pakli[19 - i * 3];
-                 pakli[i+14] = pakli[18 - i * 3];
-            }
+        switch (oszlop) {
+            case 1:
+                for (int i = 0; i < 7; i++) {
+                    pakli[i] = pakli[19 - i * 3];
+                    pakli[i + 7] = pakli[18 - i * 3];
+                    pakli[i + 14] = pakli[20 - i * 3];
+                }
+                break;
+            case 2:
+                for (int i = 0; i < 7; i++) {
+                    pakli[i] = pakli[18 - i * 3];
+                    pakli[i + 7] = pakli[19 - i * 3];
+                    pakli[i + 14] = pakli[20 - i * 3];
+                }
+                break;
+            case 3:
+                for (int i = 0; i < 7; i++) {
+                    pakli[i] = pakli[20 - i * 3];
+                    pakli[i + 7] = pakli[19 - i * 3];
+                    pakli[i + 14] = pakli[18 - i * 3];
+                }
+                break;
 
         }
-        
-       kiiratas();
+
+        kiiratas();
 
     }
 
     private static void kirak() {
         String[] szinek = {"P", "Z", "T", "M"};
         String[] szamok = {"V", "VI", "VII", "VIII", "IX", "X"};
-        
+
         for (int i = 0; i < pakli.length; i++) {
-           
-            pakli[i] = szinek[i/6]+" - "+szamok[i%6];
+
+            pakli[i] = szinek[i / 6] + " - " + szamok[i % 6];
         }
-        
-      
+
     }
 
     private static void ezvolt() {
-        System.out.println("Ez volt az általad kiválaszott lap?: "+pakli[11]);
+        System.out.println("\nEz volt az általad kiválaszott lap?: " + pakli[11]);
     }
 
 }
