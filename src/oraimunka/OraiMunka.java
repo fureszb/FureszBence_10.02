3aa4297f8ba921598a44d920c317e3ce09f2662a
fureszb <fureszb@gmail.com>
2023-10-02 09:27:01 +0200

    keveres pakli listaba helyezese

--- src/oraimunka/OraiMunka.java
+++ src/oraimunka/OraiMunka.java
@@ -5,22 +5,28 @@ import java.util.Scanner;
 public class OraiMunka {
 
     static String[] pakli = new String[22];
-
+    
     public static void main(String[] args) {
-       kirak();
-        //kiiratas();
-       // bekeres();
+        kirak();
+        kiiratas();
+        
+        for (int i = 0; i < 3; i++) {
+            int bekert = bekeres();
+            keveres(bekert);
+        }
+        ezvolt();
+       
 
-        //keveres();
+        
         
     }
 
     private static int bekeres() {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Melyik oszlopba van a kártyád?(1-3): ");
+        System.out.print("\nMelyik oszlopba van a kártyád?(1-3): ");
         int oszlop = sc.nextInt();
-        while (!(oszlop < 1 && oszlop > 3)) {
-            System.out.print("Melyik oszlopba van a kártyád?(1-3): ");
+        while (!((oszlop > 0) && (oszlop < 4))) {
+            System.out.print("HIBA \n Melyik oszlopba van a kártyád?(1-3): ");
             oszlop = sc.nextInt();
         }
         return oszlop;
@@ -28,16 +34,28 @@ public class OraiMunka {
     }
 
     private static void kiiratas() {
-        
+          int sortores = 0;
+        for (int i = 0; i < pakli.length; i++) {
+            System.out.printf(pakli[i] + "\t"+"|"+ "\t");
+            sortores++;
+            if (sortores == 3) {
+                System.out.println("");
+                sortores=0;
+            }
+        }
     }
 
     private static void keveres(int oszlop) {
         if (oszlop == 1) {
-            for (int i = 1; i <= 7; i++) {
-                i = 20 - i * 3;
+            for (int i = 0; i < 7; i++) {
+                pakli[i] = pakli[20 - i * 3];
+                 pakli[i+7] = pakli[19 - i * 3];
+                 pakli[i+14] = pakli[18 - i * 3];
             }
 
         }
+        
+       kiiratas();
 
     }
 
@@ -50,15 +68,11 @@ public class OraiMunka {
             pakli[i] = szinek[i/6]+" - "+szamok[i%6];
         }
         
-        int sortores = 0;
-        for (int i = 0; i < pakli.length; i++) {
-            System.out.printf(pakli[i] + "\t"+"|"+ "\t");
-            sortores++;
-            if (sortores == 3) {
-                System.out.println("");
-                sortores=0;
-            }
-        }
+      
+    }
+
+    private static void ezvolt() {
+        System.out.println("Ez volt az általad kiválaszott lap?: "+pakli[11]);
     }
 
 }
